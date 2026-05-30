c4d43d003c7168086f90c7766557492dfe8d5649
privateVoid <ashleychiyani588@gmail.com>
2026-05-29 22:48:11 -0700

    Add initial styles and functionality for ToDO app

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('ToDO App Loaded');
+const todoInput = document.getElementById('todoInput');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+addBtn.addEventListener('click', function appenditems() {
+    const task = todoInput.value.trim();
+    if (task) {
+        const li = document.createElement('li');
+        li.textContent = task;
+        const deletebtn = document.createElement('button');
+        deletebtn.textContent = 'Delete';
+        li.appendChild(deletebtn);
+        deletebtn.classList.add('deletebtn');
+        deletebtn.addEventListener('click', function () {
+            li.remove();
+        });
+        todoList.appendChild(li);
+        todoInput.value = '';
+    }
+});
